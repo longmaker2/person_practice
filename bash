309a189a0a60a1cf2ba51b3b9ca4074ebab1e059
longmaker2 <l.deng@alustudent.com>
2022-10-21 07:23:23 -0700

    practicing how to prompt the user to enter their details

--- bash
+++ bash
@@ -1,5 +1,1 @@
 #!/bin/bash
-for file in /home/personal_practice/*
-do
-	echo "#!/bin/bash" > ${file}
-done
---
+++ greetings.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# The user will be prompted to enter their information
+
+print ("Hello " + input ("What is your name? "))
+print ("Nice to meet you " + input ("How old are you? "))
